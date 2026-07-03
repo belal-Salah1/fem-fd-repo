827a8d3b8754d732fd8009a41bbdde3daac1d845
Belal Salah <belal.salah259@gmail.com>
2026-07-04 02:37:30 +0300

    refactor: move cluster module declaration from outputs.tf to environment main.tf and update instance type

--- terraform/module/clustor/outputs.tf
+++ terraform/module/clustor/outputs.tf
@@ -10,18 +10,3 @@ output "listener_arn" {
   value = aws_lb_listener.http.arn
 }
 
-module "cluster" {
-  source = "../cluster"
-
-  security_groups = [module.network.private_security_group]
-  subnets         = module.network.private_subnets
-  name            = var.name
-  vpc_id          = module.network.vpc_id
-
-  capacity_providers = {
-    "spot" = {
-      instance_type = "t3a.medium"
-      market_type   = "spot"
-    }
-  }
-}
--- terraform/module/environment/main.tf
+++ terraform/module/environment/main.tf
@@ -15,3 +15,19 @@ module "database" {
   subnets            = module.network.database_subnets
   vpc_name           = module.network.vpc_name
 }
+
+module "cluster" {
+  source = "../clustor"
+
+  security_groups    = [module.network.private_security_group]
+  subnets            = module.network.private_subnets
+  name               = var.name
+  vpc_id             = module.network.vpc_id
+  capacity_providers = {
+    "spot" = {
+      instance_type = "t3.small"
+      market_type   = "spot"
+    }
+  }
+}
+
